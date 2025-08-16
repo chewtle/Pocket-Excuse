a5721e439c5bc803795264de598cd70e30e55559
Chewtle <marios.neofytou1408@gmail.com>
2025-08-16 12:17:00 +0300

    Initial Commit

---
+++ PocketExcuse/PocketExcuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketExcuseApp.swift
+//  PocketExcuse
+//
+//  Created by Marios Neofytou on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PocketExcuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
